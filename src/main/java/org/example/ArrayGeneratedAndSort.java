5f4f72dad52089ff2d707b0a7111f054fde67406
Cranky Bawler <6666lenia6666@mail.ru>
2025-01-23 19:30:02 +0300

    1

---
+++ src/main/java/org/example/ArrayGeneratedAndSort.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class ArrayGeneratedAndSort {
+    private Integer[] array;
+    private int size;
+
+    public ArrayGeneratedAndSort(Integer[] array, int size) {
+        this.array = array;
+        this.size = 100000;
+    }
+
+    public static Integer[] generateArray(int size) {
+        Integer[] array = new Integer[size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt();
+
+        }
+        return array;
+    }
+
+
+
+
+//
+//    public static void sortBubble(Integer[] arrayBubble) {
+//        boolean swapped;
+//        for (int i = 0; i < arrayBubble.length - 1; i++) {
+//            swapped = false;
+//            for (int j = 0; j < arrayBubble.length - 1 - i; j++) {
+//                if (arrayBubble[j] > arrayBubble[j + 1]) {
+//                    int temp = arrayBubble[j];
+//                    arrayBubble[j] = arrayBubble[j + 1];
+//                    arrayBubble[j + 1] = temp;
+//                    swapped = true;
+//                }
+//
+//            }
+//            if (!swapped) {
+//                break;
+//            }
+//        }
+//
+//    }
+//
+//    public void timer() {
+//
+//        long start = System.currentTimeMillis();
+//        sortBubble(array);
+//        System.out.println(System.currentTimeMillis() - start);
+//    }
+//
+};
--- src/main/java/org/example/IntegerListImpl.java
+++ src/main/java/org/example/IntegerListImpl.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Random;
 
 public class IntegerListImpl implements IntegerList {
     private Integer[] items;
@@ -23,151 +24,153 @@ public class IntegerListImpl implements IntegerList {
         return item;
     }
 
-  @Override
-  public Integer add(int index, int item) {
-      if (index < 0 || index > size) {
-          throw new IndexOutOfBoundsException("Такого индекса массива не существует " + index);
-      }
-      if (size == items.length) {
-          items = Arrays.copyOf(items, items.length * 2);
-      }
-      System.arraycopy(items, index, items, index + 1, size - index);
-      items[index] = item;
-      size++;
-      return item;
-  }
+    @Override
+    public Integer add(int index, int item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Такого индекса массива не существует " + index);
+        }
+        if (size == items.length) {
+            items = Arrays.copyOf(items, items.length * 2);
+        }
+        System.arraycopy(items, index, items, index + 1, size - index);
+        items[index] = item;
+        size++;
+        return item;
+    }
 
-  @Override
-  public Integer set(int index, int item) {
-      if (index < 0 || index >= size) {
-          throw new IndexOutOfBoundsException("Такого индекса массива не существует " + index);
-      }
-      items[index] = item;
-      return item;
-  }
+    @Override
+    public Integer set(int index, int item) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Такого индекса массива не существует " + index);
+        }
+        items[index] = item;
+        return item;
+    }
 
-  @Override
-  public Integer remove(Integer item) {
-      int index = -1;
-      for (int i = 0; i < size; i++) {
-          if (items[i].equals(item)) {
-              index = i;
-              break;
-          }
-      }
-      if (index == -1) {
-          throw new IllegalArgumentException("Item не найден " + item);
-      }
-      int removerItem = items[index];
-      System.arraycopy(items, index + 1, items, index, size - index - 1);
-      items[--size] = null;
-      return removerItem;
-  }
+    @Override
+    public Integer remove(Integer item) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (items[i].equals(item)) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            throw new IllegalArgumentException("Item не найден " + item);
+        }
+        int removerItem = items[index];
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
+        items[--size] = null;
+        return removerItem;
+    }
 
-  @Override
-  public Integer remove(int index) {
-      {
-          if (index < 0 || index > size) {
-
-
-              throw new IllegalArgumentException("Такого индекса массива не существует");
-          }
-          Integer removerItem = items[index];
-          System.arraycopy(items, index + 1, items, index, size - index - 1);
-          items[--size] = null;
+    @Override
+    public Integer remove(int index) {
+        {
+            if (index < 0 || index > size) {
 
 
-          return removerItem;
-      }
-  }
+                throw new IllegalArgumentException("Такого индекса массива не существует");
+            }
+            Integer removerItem = items[index];
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[--size] = null;
 
 
-  @Override
-  public boolean contains(int item) {
-      for (int i = 0; i < size; i++) {
-          if (items[i].equals(item)) {
-              return true;
-          }
-      }
-      return false;
-  }
-
-  @Override
-  public int indexOf(int item) {
-      for (int i = 0; i < size; i++) {
-          if (items[i].equals(item)) {
-              return i;
-          }
-
-      }
-      return -1;
-  }
-
-  @Override
-  public int lastIndexOf(int item) {
-      for (int i = 0; i < size; i++) {
-          if (items[i].equals(item)) {
-              return i;
-          }
-
-      }
-      return -1;
-  }
-
-  @Override
-  public Integer get(int index) {
-      if (index < 0 || index >= size) {
-          throw new IllegalArgumentException("Такого индекса не существует");
-      }
-      return items[index];
-  }
-
-  @Override
-  public boolean equals(IntegerList otherList) {
-      return Arrays.equals(this.toArray(), otherList.toArray());
-  }
+            return removerItem;
+        }
+    }
 
 
-  @Override
-  public int size() {
+    @Override
+    public boolean contains(int item) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].equals(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-      return size;
-  }
+    @Override
+    public int indexOf(int item) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].equals(item)) {
+                return i;
+            }
 
-  @Override
-  public boolean isEmpty() {
+        }
+        return -1;
+    }
 
-      return size == 0;
-  }
+    @Override
+    public int lastIndexOf(int item) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].equals(item)) {
+                return i;
+            }
 
-  @Override
-  public void clear() {
-      size = 0;
+        }
+        return -1;
+    }
 
-  }
+    @Override
+    public Integer get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Такого индекса не существует");
+        }
+        return items[index];
+    }
 
-  @Override
-  public Integer[] toArray() {
-      Integer[] result = new Integer[size];
-      System.arraycopy(items, 0, result, 0, size);
-      return result;
-  }
-
-  public Integer[] getItems() {
-      return Arrays.copyOf(items, size);
-  }
-
-  public int getSize() {
-      return size;
-  }
+    @Override
+    public boolean equals(IntegerList otherList) {
+        return Arrays.equals(this.toArray(), otherList.toArray());
+    }
 
 
-  @Override
-  public int hashCode() {
-      int result = Objects.hash(size);
-      result = 31 * result + Arrays.hashCode(items);
-      return result;
-  }
+    @Override
+    public int size() {
+
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+
+    }
+
+    @Override
+    public Integer[] toArray() {
+        Integer[] result = new Integer[size];
+        System.arraycopy(items, 0, result, 0, size);
+        return result;
+    }
+
+    public Integer[] getItems() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size);
+        result = 31 * result + Arrays.hashCode(items);
+        return result;
+    }
+
 
 }
+
 
